449b6eaa7339f35773c55a9272b87be739d185e9
ryan-doucette <doucette.ry@northeastern.edu>
2022-11-10 13:34:06 -0500

    added dislikes

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -6,22 +6,22 @@ import { useDispatch } from "react-redux";
 const TuitStats = ({post}) => {
     const dispatch = useDispatch();
     return(
-        <div className="row wd-tuit-stats-mw pt-3">
-            <div className="col d-flex">
-                <i role='button' className="bi bi-chat fa-1x pe-2 justify-self-center"/>
+        <div className="wd-tuit-stats-mw pt-3 d-flex flex-row align-content-between">
+            <div className="d-flex wd-stats-spacing">
+                <i role='button' className="bi bi-chat fa-1x justify-self-center wd-stats-icon"/>
                 <span>{post.replies}</span>
             </div>
-            <div className="col d-flex">
-                <i role='button' className="bi bi-arrow-repeat fa-1x pe-2 justify-self-center"/>
+            <div className="d-flex wd-stats-spacing">
+                <i role='button' className="bi bi-arrow-repeat fa-1x wd-stats-icon justify-self-center"/>
                 <span>{post.retuits}</span>
             </div>
-            <div className="col d-flex">
-                <span className="pe-2">
+            <div className="d-flex wd-stats-spacing">
+                <span className="wd-liked-and-disliked">
                     Likes:
                 </span>
                 <i 
                     role='button' 
-                    className={`${post.liked === "true" ? "bi-heart-fill text-danger" : "bi-heart"} me-2 bi fa-1x justify-self-center`}
+                    className={`${post.liked === "true" ? "bi-heart-fill text-danger" : "bi-heart"} wd-stats-icon bi fa-1x justify-self-center`}
                     onClick={() => 
                         dispatch(updateTuitThunk({
                             ...post,   
@@ -32,7 +32,24 @@ const TuitStats = ({post}) => {
                 />
                 <span>{post.likes}</span>
             </div>
-            <i role='button' className="col bi bi-share fa-1x pe-2 justify-self-center"></i>
+            <div className="d-flex wd-stats-spacing">
+                <span className="wd-liked-and-disliked">
+                    Dislikes:
+                </span>
+                <i 
+                    role='button' 
+                    className={`${post.disliked === "true" ? "bi bi-hand-thumbs-down-fill text-primary" : "bi bi-hand-thumbs-down"} wd-stats-icon bi fa-1x justify-self-center`}
+                    onClick={() => 
+                        dispatch(updateTuitThunk({
+                            ...post,   
+                            disliked: post.disliked === 'true' ? 'false' : 'true',
+                            dislikes: post.disliked !== 'true' ? post.dislikes + 1 : post.dislikes - 1
+                        }))
+                    } 
+                />
+                <span>{post.dislikes}</span>
+            </div>
+            <i role='button' className="bi bi-share fa-1x wd-stats-icon justify-self-center wd-stats-spacing"></i>
         </div>
     );
 };
